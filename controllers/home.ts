7cb41a5c21d1749b7817063cfa258e996741eaf9
vichule <javier.cabanasdiaz@gmail.com>
2024-04-03 19:21:03 +0200

    config/instalation for serverless and deployment

--- controllers/home.ts
+++ controllers/home.ts
@@ -4,5 +4,5 @@ import path from "path";
 export const homeRouter = express.Router();
 
 homeRouter.get('/', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname + '/../index.html'));
+    res.sendFile(path.resolve(process.cwd(), 'index.html'));
 });
