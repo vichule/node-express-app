c86fd256ef15bc87af9da66f3ff7a1f1f95d8f67
vichule <javier.cabanasdiaz@gmail.com>
2024-04-25 23:22:57 +0200

    delete booking service

--- services/booking.ts
+++ services/booking.ts
@@ -1,6 +1,8 @@
 import { BookingInterface } from "../interfaces/Booking";
 import { ErrorApp } from "../classes/ErrorApp";
 import { bookingModel } from "../models/BookingModel";
+import { connect, disconnect } from "../util/connection";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 
 
@@ -25,16 +27,21 @@ export const getBooking = async (id: any): Promise<BookingInterface | null> => {
 
 }
 
-export const deleteBooking = async (id: any): Promise<BookingInterface | null> => {
-    const bookingData = (await bookingModel.findByIdAndDelete(id))
-
-    if (bookingData === null){
+export const deleteBooking = async (id: any): Promise<RowDataPacket> => {
+    
+    const conn = await connect()
+    const query = `DELETE FROM bookings where id = ?`
+    const prepared = await conn.prepare(query)
+    const [results, fields] = await prepared.execute([id])
+    const resultHeaders = results as ResultSetHeader
+    const bookingData = results as RowDataPacket
+    conn.unprepare(query)
+    disconnect(conn)
+    if (resultHeaders.affectedRows === 0) {
         throw new ErrorApp({ status: 404, message: 'Error, booking does not exist' })
-    }else{
+    } else {
         return bookingData
     }
-    
-
 
 
 }
