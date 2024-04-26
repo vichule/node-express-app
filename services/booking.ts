653d11b4aa7241b2e64a4b5153d193521bf21481
vichule <javier.cabanasdiaz@gmail.com>
2024-04-26 08:25:25 +0200

    getOne & getAll booking services

--- services/booking.ts
+++ services/booking.ts
@@ -10,17 +10,41 @@ import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 
 export const getBookings = async (): Promise<BookingInterface[]> => {
-        return await bookingModel.find({}).populate('room')
+    const conn = await connect()
+
+    const [results, fields] = await conn.execute(`SELECT 
+    b.id AS booking_id, b.first_name, b.last_name, b.order_date, 
+    b.check_in, b.check_out, b.notes, b.status AS booking_status,
+    b.discount AS booking_discount, r.id AS room_id, r.room_type, 
+    r.room_number, r.description AS room_description, r.price AS room_price, r.offer, r.discount AS room_discount, r.cancellation, r.status AS room_status
+    FROM bookings b
+    JOIN rooms r ON b.room_id = r.id;`)
+    const bookings = results as BookingInterface[]
+    disconnect(conn)
+    return bookings
 
 }
 
-export const getBooking = async (id: any): Promise<BookingInterface | null> => {
+export const getBooking = async (id: any): Promise<RowDataPacket> => {
 
-        const bookingData = (await bookingModel.findById(id).populate('room'))
-    if (bookingData === null) {
+    const conn = await connect()
+    const query = `SELECT 
+    b.id AS booking_id, b.first_name, b.last_name, b.order_date, 
+    b.check_in, b.check_out, b.notes, b.status AS booking_status,
+    b.discount AS booking_discount, r.id AS room_id, r.room_type, 
+    r.room_number, r.description AS room_description, r.price AS room_price, r.offer, r.discount AS room_discount, r.cancellation, r.status AS room_status
+    FROM bookings b
+    JOIN rooms r ON b.room_id = r.id
+    WHERE r.id = ?;`
+    const prepared = await conn.prepare(query)
+    const [results, fields] = await prepared.execute([id])
+    const bookingsData = results as RowDataPacket
+    conn.unprepare(query)
+    disconnect(conn)
+    if (bookingsData.length === 0) {
         throw new ErrorApp({ status: 404, message: 'Error, booking does not exist' })
-    }else{
-        return bookingData
+    } else {
+        return bookingsData
     }
 
 
